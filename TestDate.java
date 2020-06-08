442df1f56a062720e2955a9695d15bdcc399dae1
Valou <devcodpanda@gmail.com>
2020-06-08 11:37:00 +0200

    Maybe

--- TestDate.java
+++ TestDate.java
@@ -1,12 +1,14 @@
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Scanner;
 import java.util.Date;
-import java.util.Scanner; 
+import java.text.ParseException;
+
 
 class TestDate {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Please fill a day (1-31) :");
@@ -18,6 +20,14 @@ class TestDate {
         sc.close();
 
         // TODO : your code after this line
-        
-    }
-}
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        Calendar c = new GregorianCalendar(year, month -1, day);
+        Date d = c.getTime();
+
+        int dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+
+        System.out.println("The date is : " + sdf.format(d);
+        System.out.println("The day of week is : " + c.get(Calendar.DAY_OF_WEEK));
+        }
+        }
